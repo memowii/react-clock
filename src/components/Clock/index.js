8baaf7c4311e5698e34a3eb2b0c06abbb1262141
memowii <memogl25@gmail.com>
2020-08-15 20:00:32 -0500

    Add functions getCurrentTime and formatTimeUnit

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./index.css";
 
 export function Clock() {
+  const [time, setTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(getCurrentTime());
+    }, 1000);
+  }, [time]);
+
   return (
     <div className="Clock">
-      <div className="Clock__time">19:11:07</div>
+      <div className="Clock__time">{time}</div>
       <div className="Clock__date">Sat 16 August 2020</div>
     </div>
   );
+}
+
+function getCurrentTime() {
+  const date = new Date();
+  const hours = formatTimeUnit(date.getHours());
+  const minutes = formatTimeUnit(date.getMinutes());
+  const seconds = formatTimeUnit(date.getSeconds());
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+
+  return currentTime;
+}
+
+function formatTimeUnit(timeUnit) {
+  return Math.floor(timeUnit / 10) === 0 ? `0${timeUnit}` : timeUnit;
 }
