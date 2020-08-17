c6b98d861d3af9a99af12d15729ab7660de6dcc0
memowii <memogl25@gmail.com>
2020-08-17 18:09:40 -0500

    Add logic for Clock component

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,34 +1,32 @@
 import React, { useState, useEffect } from "react";
+import dayjs from "dayjs";
 
 import "./index.css";
 
 export function Clock() {
-  const [time, setTime] = useState(getCurrentTime());
+  const currentDate = getDate();
+  const [time, setTime] = useState(currentDate.time);
+  const [date, setDate] = useState(currentDate.date);
 
   useEffect(() => {
     setTimeout(() => {
-      setTime(getCurrentTime());
+      const currentDate = getDate();
+      setTime(currentDate.time);
+      setDate(currentDate.date);
     }, 1000);
   }, [time]);
 
   return (
     <div className="Clock">
       <div className="Clock__time">{time}</div>
-      <div className="Clock__date">Sat 16 August 2020</div>
+      <div className="Clock__date">{date}</div>
     </div>
   );
 }
 
-function getCurrentTime() {
-  const date = new Date();
-  const hours = formatTimeUnit(date.getHours());
-  const minutes = formatTimeUnit(date.getMinutes());
-  const seconds = formatTimeUnit(date.getSeconds());
-  const currentTime = `${hours}:${minutes}:${seconds}`;
-
-  return currentTime;
-}
-
-function formatTimeUnit(timeUnit) {
-  return Math.floor(timeUnit / 10) === 0 ? `0${timeUnit}` : timeUnit;
+function getDate() {
+  return {
+    time: dayjs().format('HH:mm:ss'),
+    date: dayjs().format('ddd D MMMM YYYY')
+  }
 }
